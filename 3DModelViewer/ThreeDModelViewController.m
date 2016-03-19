4215bd06932f71f22bdab971ad4104279b5a2de5
Shekhar Sasikumar <shekkharan@gmail.com>
2016-03-19 18:42:40 +0800

    Setup OpenGL ES 2.0 context and set GLKView and its delegate

--- 3DModelViewer/ThreeDModelViewController.m
+++ 3DModelViewer/ThreeDModelViewController.m
@@ -10,4 +10,21 @@
 
 @implementation ThreeDModelViewController
 
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+  
+  EAGLContext *context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+  [EAGLContext setCurrentContext:context];
+  
+  GLKView *glkview = (GLKView *)self.view;
+  glkview.context = context;
+  glClearColor(0.7f, 0.8f, 1.0f, 1.0f);
+}
+
+- (void)glkView:(GLKView *)view drawInRect:(CGRect)rect
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+}
+
 @end
