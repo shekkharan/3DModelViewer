76b361058a41fa5a369ad3e44b1d6358028da95d
Shekhar Sasikumar <shekkharan@gmail.com>
2016-03-20 12:18:23 +0800

    added pan, pinch and rotate gestures + transformation manager functionalities

--- 3DModelViewer/ThreeDModelViewController.m
+++ 3DModelViewer/ThreeDModelViewController.m
@@ -8,6 +8,8 @@
 
 #import "ThreeDModelViewController.h"
 #import "ThreeDModel.h"
+#import "TransformationManager.h"
+
 @interface ThreeDModelViewController ()
 {
   GLKVector3  rotationStartVector;
@@ -15,7 +17,7 @@
 
 @property (strong, nonatomic) GLKBaseEffect *baseEffect;
 @property (strong, nonatomic) ThreeDModel *threeDModel;
-
+@property (strong, nonatomic) TransformationManager *transformationManager;
 @end
 
 @implementation ThreeDModelViewController
@@ -27,12 +29,13 @@
   EAGLContext *context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
   [EAGLContext setCurrentContext:context];
   
-  GLKView *glkview = (GLKView *)self.view;
-  glkview.context = context;
+  GLKView *glkView = (GLKView *)self.view;
+  glkView.context = context;
   glClearColor(0.7f, 0.8f, 1.0f, 1.0f);
   glEnable(GL_CULL_FACE);
   [self setupBaseEffect];
   self.threeDModel = [[ThreeDModel alloc] initWithModel:kThreeDModelChair];
+  self.transformationManager = [[TransformationManager alloc] initWithRotation:GLKVector3Make(0.0f, 0.0f, 0.0f) translation:GLKVector2Make(0.0f, 0.0f) scale:2.0f andDepth:3.0f];
 }
 
 - (void)setupBaseEffect
@@ -48,24 +51,57 @@
   const GLfloat fieldView = GLKMathDegreesToRadians(90.0f);
   const GLKMatrix4 projectionMatrix = GLKMatrix4MakePerspective(fieldView, aspectRatio, 0.1f, 10.0f);
   self.baseEffect.transform.projectionMatrix = projectionMatrix;
-  
-  GLKMatrix4 modelViewMatrix = GLKMatrix4Identity;
-  modelViewMatrix = GLKMatrix4Translate(modelViewMatrix, 0.0f, 0.0f, -5.0f);
-  modelViewMatrix = GLKMatrix4RotateX(modelViewMatrix, GLKMathDegreesToRadians(45.0f));
+
+  GLKMatrix4 modelViewMatrix = [self.transformationManager getModelViewMatrix];
   self.baseEffect.transform.modelviewMatrix = modelViewMatrix;
 }
 
 - (void)glkView:(GLKView *)view drawInRect:(CGRect)rect
 {
-  glClear(GL_COLOR_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   [self.baseEffect prepareToDraw];
   [self setupMatrices];
   [self.threeDModel renderModel];
 }
 
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+  [self.transformationManager reset];
+}
+
 - (IBAction)pan:(UIPanGestureRecognizer *)sender {
-  CGPoint rotation = [sender translationInView:sender.view];
-  NSLog(@"Rotation %f,%f", rotation.x, rotation.y);
+  if((sender.numberOfTouches == 1) &&
+          ((self.transformationManager.state == TransformationStateNew) || (self.transformationManager.state == TransformationStateRotation)))
+  {
+    const float multipier = GLKMathDegreesToRadians(1.2f);
+    CGPoint rotation = [sender translationInView:self.view];
+    [self.transformationManager rotateWith:GLKVector3Make(rotation.x, rotation.y, 0.0f) andMultiplier:multipier];
+    
+  } else if((sender.numberOfTouches == 2) &&
+     ((self.transformationManager.state == TransformationStateNew) || (self.transformationManager.state == TransformationStateTranslation)))
+  {
+    CGPoint translation = [sender translationInView:self.view];
+    float x = translation.x/sender.view.frame.size.width;
+    float y = translation.y/sender.view.frame.size.height;
+    [self.transformationManager translateWith:GLKVector2Make(x, y) andMultiplier:5.0f];
+  }
+}
+
+- (IBAction)pinch:(UIPinchGestureRecognizer *)sender {
+  if((self.transformationManager.state == TransformationStateNew) || (self.transformationManager.state == TransformationStateScale))
+  {
+    float scale = [sender scale];
+    [self.transformationManager scaleWith:scale];
+  }
+  
+}
+
+- (IBAction)rotation:(UIRotationGestureRecognizer *)sender {
+  if((self.transformationManager.state == TransformationStateNew) || (self.transformationManager.state == TransformationStateRotation))
+  {
+    float rotation = [sender rotation];
+    [self.transformationManager rotateWith:GLKVector3Make(0.0f, 0.0f, rotation) andMultiplier:1.0f];
+  }
 }
 
 @end
